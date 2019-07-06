a0eff9c317c9fbcfe81c67b01c5c4920ccc6fe61
Elfrey Shira <elf.shira@gmail.com>
2019-07-05 22:59:23 -0500

    make it good

--- src/App.js
+++ src/App.js
@@ -37,7 +37,8 @@ export default React.createClass({
         <form onSubmit={this.doStuff}>
           <input ref="elfrey" type="text" />
         </form>
-        <button onClick={() => this.show()}>Done</button>
+        <button onClick={(evt) => this.doStuff(evt)}>Done</button>
+        <button onClick={() => this.show()}>Start</button>
         {
           this.state.show ?
             _.map(_.shuffle(this.state.list), (lol) => {
